638319eb56623fce2c7c37140a2c697a847fa916
Jon Shawhan <73123430+ShawhanJon@users.noreply.github.com>
2023-02-16 14:53:25 +0000

    Learned Data Types

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #Data Types
+
+#String
+#pulling character from string is called subscripting
+#numbers in quotes are treated as strings
+
+"Hello"[4]
+
+#Integer
+#Whole numbers
+
+print(123 + 345)
+
+#Float
+#decimal numbers short for a floating point number
+
+#Boolean
+#True or False
