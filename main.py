339c3d96a8f4e98a783fe43546ca51e8e2e2e410
Jon Shawhan <73123430+ShawhanJon@users.noreply.github.com>
2023-03-12 18:49:22 +0000

    Completed Casting exercise

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 #Data Types
 
 #String
-#pulling character from string is called subscripting
+#pulling character from string is called subscripting, using []
 #numbers in quotes are treated as strings
 
-"Hello"[4]
+# print("Hello"[4])
 
 #Integer
 #Whole numbers
 
-print(123 + 345)
+# print(123 + 345)
 
 #Float
 #decimal numbers short for a floating point number
 
 #Boolean
 #True or False
+
+###Casting Data types
+# num_char = len(input("What's your name? "))
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+## type() gives the type of the object
+## str(), float(), int(), etc casts to new type
+
+# a = float(123)
+# print(type(a))
+# print(70 + float("100.5"))
